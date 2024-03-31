5ef8b6f615766ed6dc6038a1d2aa84437d0b0d45
Matteo Manzoni <matteo.manzoni2@studenti.unimi.it>
2024-03-31 15:36:20 +0200

    Add docs in Renderer

--- Assets/Scripts/Renderer.cs
+++ Assets/Scripts/Renderer.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 
+/*
+    This script is used to set the correct render pipeline asset at the beginning of the game
+    The pipeline could also be set from Edit-> Project Settings -> Graphics -> Scriptable Render Pipeline Settings,
+    but this script is useful if you don't want it to be set in the editor (hiding enemies for example)
+ */
 public class Renderer : MonoBehaviour
 {
     [SerializeField] private RenderPipelineAsset renderPipelineAsset;
