f3e266cf85c2b897c7ed5382fa2a1adb1fd61a65
Christian Pozzoli <pozzoli.christian99@gmail.com>
2024-04-05 00:53:00 +0200

    Actions for movement and view orientation for both kayboard and gamepad

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,27 +1,77 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5F;
-    
+
+    private CustomInput input = null;
     private Rigidbody rb;
+
     private Vector3 movement;
-    
-    
+    private Vector3 direction;
+    private Camera camera;
+
+    private void Awake()
+    {
+        input = new CustomInput();
+    }
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
-    
-    private void Update()
-    {
-        movement = speed * new Vector3(Input.GetAxis("Horizontal"), 0F, Input.GetAxis("Vertical"));
+        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
     
     private void FixedUpdate()
     {
-        //rb.MovePosition(transform.position + movement);
-        transform.position += movement;
+        rb.MovePosition(transform.position + speed * Time.fixedDeltaTime * movement);
+        rb.MoveRotation(Quaternion.AngleAxis(Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg, transform.up));
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+        input.Player.Movement.performed += Movement_performed;
+        input.Player.Movement.canceled += Movement_canceled;
+
+        input.Player.MousePosition.performed += MousePosition_performed;
+        input.Player.View.performed += View_performed;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+        input.Player.Movement.performed -= Movement_performed;
+        input.Player.Movement.canceled -= Movement_canceled;
+        
+        input.Player.MousePosition.performed -= MousePosition_performed;
+        input.Player.View.performed -= View_performed;
+    }
+
+    private void Movement_performed(InputAction.CallbackContext callbackContext)
+    {
+        Vector2 temp = callbackContext.ReadValue<Vector2>();
+        movement = new Vector3(temp.x, 0, temp.y);
+    }
+
+    private void Movement_canceled(InputAction.CallbackContext callbackContext)
+    {
+        movement = Vector3.zero;
+    }
+
+    private void MousePosition_performed(InputAction.CallbackContext callbackContext)
+    {
+        Vector3 mousePos = callbackContext.ReadValue<Vector2>();
+        mousePos.z = camera.transform.position.y - transform.position.y;
+        Vector3 lookPosition = camera.ScreenToWorldPoint(mousePos);
+        direction = (lookPosition - transform.position).normalized;
+    }
+
+    private void View_performed(InputAction.CallbackContext callbackContext)
+    {
+        Vector3 temp = callbackContext.ReadValue<Vector2>();
+        direction = new Vector3(temp.x, 0, temp.y);
     }
 }
