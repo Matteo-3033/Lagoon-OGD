118fc1187f4e67d6e78c88ea58e326f1ba611de8
Christian Pozzoli <pozzoli.christian99@gmail.com>
2024-04-06 15:43:26 +0200

    Player movement and orientation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,21 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 5F;
+    [SerializeField] private float maxSpeed = 10F;
+    [Space]
+    [SerializeField] private bool acceleratedMovement = true;
+    [SerializeField] private float acceleration = 50F;
+    [SerializeField] private float deacceleration = 25F;
+    [Space]
+    [SerializeField] private float rotationSpeed = 5F;
 
     private CustomInput input = null;
     private Rigidbody rb;
+    private Camera mainCamera;
 
-    private Vector3 movement;
-    private Vector3 direction;
-    private Camera camera;
+    private Vector3 currentSpeed;
+    private Vector3 inputMovementDirection;
+    private Vector3 lookDirection;
 
     private void Awake()
     {
@@ -21,13 +28,68 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
-    
+
     private void FixedUpdate()
     {
-        rb.MovePosition(transform.position + speed * Time.fixedDeltaTime * movement);
-        rb.MoveRotation(Quaternion.AngleAxis(Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg, transform.up));
+        if (acceleratedMovement)
+        {
+            AcceleratedMovement();
+        }
+        else
+        {
+            InstantMovement();
+        }
+
+        LookRotation();
+    }
+
+    private void LookRotation()
+    {
+        float angle = Mathf.Atan2(lookDirection.x, lookDirection.z) * Mathf.Rad2Deg;
+        float step = rotationSpeed * Time.fixedDeltaTime;
+        rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, angle, 0), step));
+    }
+
+    private void AcceleratedMovement()
+    {
+        float t = Time.fixedDeltaTime;
+        Vector3 accelerationComponent = inputMovementDirection * acceleration;
+        if (inputMovementDirection.magnitude <= .1f)
+        {
+            if (currentSpeed.magnitude <= deacceleration * t)
+            {
+                currentSpeed = Vector3.zero;
+            }
+            else
+            {
+                accelerationComponent -= currentSpeed.normalized * deacceleration;
+            }
+        }
+
+        float speedLimit = inputMovementDirection.magnitude != 0 ? maxSpeed * inputMovementDirection.magnitude : maxSpeed; //Speed is limited by the controller analogue
+        Vector3 movement = currentSpeed * t + .5f * t * t * accelerationComponent;
+        currentSpeed = Vector3.ClampMagnitude(currentSpeed + t * accelerationComponent, speedLimit);
+
+        Debug.DrawRay(transform.position, currentSpeed, Color.cyan);
+
+        rb.MovePosition(transform.position + movement);
+        Debug.Log("Speed: " + movement.magnitude / t + " m/s");
+    }
+
+    private void InstantMovement()
+    {
+        float t = Time.fixedDeltaTime;
+
+        float speedLimit = inputMovementDirection.magnitude != 0 ? maxSpeed * inputMovementDirection.magnitude : maxSpeed; //Speed is limited by the controller analogue
+        Vector3 movement = inputMovementDirection * t * speedLimit;
+
+        Debug.DrawRay(transform.position, movement * 10, Color.green);
+        Debug.DrawRay(transform.position, currentSpeed, Color.cyan);
+
+        rb.MovePosition(transform.position + movement);
+        Debug.Log("Speed: " + movement.magnitude / t + " m/s");
     }
 
     private void OnEnable()
@@ -45,7 +107,7 @@ public class PlayerController : MonoBehaviour
         input.Disable();
         input.Player.Movement.performed -= Movement_performed;
         input.Player.Movement.canceled -= Movement_canceled;
-        
+
         input.Player.MousePosition.performed -= MousePosition_performed;
         input.Player.View.performed -= View_performed;
     }
@@ -53,25 +115,25 @@ public class PlayerController : MonoBehaviour
     private void Movement_performed(InputAction.CallbackContext callbackContext)
     {
         Vector2 temp = callbackContext.ReadValue<Vector2>();
-        movement = new Vector3(temp.x, 0, temp.y);
+        inputMovementDirection = new Vector3(temp.x, 0, temp.y);
     }
 
     private void Movement_canceled(InputAction.CallbackContext callbackContext)
     {
-        movement = Vector3.zero;
+        inputMovementDirection = Vector3.zero;
     }
 
     private void MousePosition_performed(InputAction.CallbackContext callbackContext)
     {
         Vector3 mousePos = callbackContext.ReadValue<Vector2>();
-        mousePos.z = camera.transform.position.y - transform.position.y;
-        Vector3 lookPosition = camera.ScreenToWorldPoint(mousePos);
-        direction = (lookPosition - transform.position).normalized;
+        mousePos.z = mainCamera.transform.position.y - transform.position.y;
+        Vector3 lookPosition = mainCamera.ScreenToWorldPoint(mousePos);
+        lookDirection = (lookPosition - transform.position).normalized;
     }
 
     private void View_performed(InputAction.CallbackContext callbackContext)
     {
         Vector3 temp = callbackContext.ReadValue<Vector2>();
-        direction = new Vector3(temp.x, 0, temp.y);
+        lookDirection = new Vector3(temp.x, 0, temp.y);
     }
 }
