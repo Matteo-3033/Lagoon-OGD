6e6221c9baf4d816f47a06d692f7beef0ae47f98
Matteo Manzoni <matteo.manzoni2@studenti.unimi.it>
2024-04-16 20:07:22 +0200

    event logger implementation

---
+++ Assets/Scripts/Round/UI/Main/EventLogger.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Round.UI.Main
+{
+    public class EventLogger : MonoBehaviour
+    {
+        [FormerlySerializedAs("eventLogPrefab")] [SerializeField] private EventLoggerText eventLogTemplate;
+        [SerializeField] private float onScreenDuration = 5F;
+        
+        private readonly ConcurrentQueue<string> eventQueue = new();
+        
+        private float height;
+        
+        private bool canSpawn = true;
+        private bool CanSpawn
+        {
+            get => canSpawn;
+            set
+            {
+                canSpawn = value;
+                if (!canSpawn) return;
+                if (eventQueue.TryDequeue(out var msg))
+                    LogEvent(msg);
+            }
+        }
+
+        private void Awake()
+        {
+            height = GetComponent<RectTransform>().rect.height;
+            eventLogTemplate.gameObject.SetActive(false);
+        }
+
+        [ContextMenu("Test event")]
+        public void LogEvent()
+        {
+            LogEvent("Test event");
+        }
+
+        private void LogEvent(string msg)
+        {
+            if (!CanSpawn)
+                eventQueue.Enqueue(msg);
+            else
+                StartCoroutine(SpawnEvent(msg));
+        }
+
+        private IEnumerator SpawnEvent(string msg)
+        {
+            CanSpawn = false;
+            var eventLog = Instantiate(eventLogTemplate, transform);
+            
+            var eventLogTransform = eventLog.GetComponent<RectTransform>();
+            eventLogTransform.anchorMax = new Vector2(0.5F, 0);
+            eventLogTransform.anchorMin = new Vector2(0.5F, 0);
+            eventLogTransform.pivot = new Vector2(0.5F, 0);
+            eventLogTransform.anchoredPosition = new Vector2(0.5F, 0);
+            
+            var maxHeight = height - eventLogTransform.rect.height;
+
+            // Wait one frame for the event log to be fully instantiated
+            yield return null;
+
+            eventLog.gameObject.SetActive(true);
+            eventLog.OnHeightSurpassed += () => CanSpawn = true;
+            eventLog.Init(msg, maxHeight, onScreenDuration);
+        }
+    }
+}
---
+++ Assets/Scripts/Round/UI/Main/EventLoggerText.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Round.UI.Main
+{
+    [RequireComponent(typeof(TextMeshProUGUI)), RequireComponent(typeof(RectTransform))]
+    public class EventLoggerText: MonoBehaviour
+    {
+        [SerializeField] private float fadeOutAfterSeconds = 1F;
+        
+        private RectTransform rectTransform;
+        private TextMeshProUGUI text;
+        
+        private float Height => rectTransform.rect.height;
+        private Vector2 endPosition;
+        
+        
+        public event Action OnHeightSurpassed;
+        
+        private void Awake()
+        {
+            rectTransform = GetComponent<RectTransform>();
+            text = GetComponent<TextMeshProUGUI>();
+        }
+        
+        public void Init(string msg, float endHeight, float duration)
+        {
+            text.text = msg;
+            
+            StartCoroutine(FadeOut(duration, endHeight));
+        }
+
+        private IEnumerator FadeOut(float duration, float endHeight)
+        {
+            yield return new WaitForSeconds(fadeOutAfterSeconds);
+            
+            var elapsedTime = 0F;
+            var color = text.color;
+            
+            var startPosition = rectTransform.anchoredPosition;
+            var endPosition = new Vector2(rectTransform.anchoredPosition.x, endHeight);
+            
+            while (elapsedTime < duration)
+            {
+                elapsedTime += Time.deltaTime;
+                
+                var newPosition = Vector3.Lerp(startPosition, endPosition, elapsedTime / duration);
+                if (newPosition.y - startPosition.y > Height && rectTransform.anchoredPosition.y - startPosition.y <= Height)
+                    OnHeightSurpassed?.Invoke();
+                rectTransform.anchoredPosition = newPosition;
+                
+                color.a = Mathf.Lerp(1, 0, elapsedTime / duration);
+                text.color = color;
+                
+                yield return null;
+            }
+            
+            Destroy(gameObject);
+        }
+    }
+}
