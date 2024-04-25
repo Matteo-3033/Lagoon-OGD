fcd544930d9b9bc6c8e55e9a6b2374c2b2e50fbe
Matteo Manzoni <matteo.manzoni2@studenti.unimi.it>
2024-04-25 23:01:14 +0200

    search match ui

--- Assets/Scripts/MainMenu/TabletUIManager.cs
+++ Assets/Scripts/MainMenu/TabletUIManager.cs
@@ -1,11 +1,47 @@
-﻿using UnityEngine;
+﻿
+
+using System.Collections.Generic;
+using UnityEngine;
+using Screen = Utils.Screen;
 
 namespace MainMenu
 {
     public class TabletUIManager: MonoBehaviour
     {
-        public void ShowMenu(UIManager.MenuKey mainMenu)
+        [SerializeField] private Screen mainMenu;
+        [SerializeField] private Screen searchMatchMenu;
+        
+        private readonly Dictionary<UIManager.MenuKey, Screen> menus = new();
+        private Screen currentMenu;
+        
+        private void Start()
         {
+            if (mainMenu == null)
+            {
+                Debug.LogError("UIManager: Main Menu is not set.");
+                return;
+            }
+            
+            AddMenu(UIManager.MenuKey.MainMenu, mainMenu);
+            AddMenu(UIManager.MenuKey.SearchingMatch, searchMatchMenu);
+            
+            ShowMenu(UIManager.MenuKey.MainMenu);
+        }
+        
+        private void AddMenu(UIManager.MenuKey key, Screen value)
+        {
+            if (value != null)
+                menus.Add(key, value);
+            value.gameObject.SetActive(false);
+        }
+
+        public void ShowMenu(UIManager.MenuKey menuKey)
+        {
+            if (currentMenu != null)
+                currentMenu.OnUnfocus();
+            
+            currentMenu = menus[menuKey];
+            currentMenu.OnFocus();
         }
     }
 }
