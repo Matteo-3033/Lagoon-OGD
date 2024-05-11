0e7ab5dee37dc690fdc66f1b47e4574b4f32cb53
Matteo Manzoni <matteo.manzoni2@studenti.unimi.it>
2024-05-11 21:25:45 +0200

    soundtrack speed increase when time is running out

--- Assets/Scripts/Audio/SoundtrackManager.cs
+++ Assets/Scripts/Audio/SoundtrackManager.cs
@@ -1,12 +1,16 @@
 using Round;
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace Audio
 {
     [RequireComponent(typeof(AudioSource))]
     public class SoundtrackManager : MonoBehaviour
     {
+        [SerializeField] private string audioMixerGroupPitchParam = "PitchBend";
+        
         private AudioSource source;
+        private bool tempoIncreased;
 
         private void Awake()
         {
@@ -25,6 +29,16 @@ namespace Audio
         {
             RoundController.Instance.OnRoundStarted += OnRoundStarted;
             RoundController.Instance.OnRoundEnded += OnRoundEnded;
+            RoundController.Instance.TimerUpdate += OnTimerUpdate;
+        }
+
+        private void OnTimerUpdate(int remainingTime)
+        {
+            if (tempoIncreased || remainingTime > 60) return;
+            
+            tempoIncreased = true;
+            source.pitch = 2F;
+            source.outputAudioMixerGroup.audioMixer.SetFloat(audioMixerGroupPitchParam, 1f / 2F);
         }
 
         private void OnRoundStarted()
