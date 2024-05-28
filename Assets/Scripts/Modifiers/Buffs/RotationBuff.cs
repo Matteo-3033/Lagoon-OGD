1d35425608766ce855490b323a6288d5eaabf118
Matteo Manzoni <matteo.manzoni2@studenti.unimi.it>
2024-05-28 10:38:39 +0200

    rotation buff

---
+++ Assets/Scripts/Modifiers/Buffs/RotationBuff.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Modifiers.Buffs
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/Buffs/Rotation", fileName = "RotationBuff")]
+    public class RotationBuff : StatsModifier
+    {
+        public float percentIncrease = 25;
+        
+        public override void Enable()
+        {
+            Player.LocalPlayer.RotationController.AddPercentage(percentIncrease);
+        }
+
+        public override void Disable()
+        {
+            Player.LocalPlayer.RotationController.SubPercentage(percentIncrease);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerRotationController.cs
+++ Assets/Scripts/Player/PlayerRotationController.cs
@@ -1,11 +1,9 @@
-using Mirror;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using Utils;
 
 public class PlayerRotationController : MonoBehaviour
 {
-    [SerializeField] private float rotationSpeed = 800f;
+    [SerializeField] private float baseRotationSpeed = 800f;
+    private float RotationSpeed { get; set; }
 
     private Rigidbody rb;
     private IInputHanlder inputHandler;
@@ -14,6 +12,7 @@ public class PlayerRotationController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         inputHandler = GetComponentInParent<IInputHanlder>();
+        RotationSpeed = baseRotationSpeed;
     }
 
     // Update is called once per frame
@@ -26,7 +25,17 @@ public class PlayerRotationController : MonoBehaviour
     private void LookRotation(Vector3 lookDirection)
     {
         float angle = Mathf.Atan2(lookDirection.x, lookDirection.z) * Mathf.Rad2Deg;
-        float step = rotationSpeed * Time.fixedDeltaTime;
+        float step = RotationSpeed * Time.fixedDeltaTime;
         rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, angle, 0), step));
+    }
+    
+    public void AddPercentage(float percent)
+    {
+        RotationSpeed += baseRotationSpeed * percent / 100;
+    }
+    
+    public void SubPercentage(float percent)
+    {
+        RotationSpeed -= baseRotationSpeed * percent / 100;
     }
 }
