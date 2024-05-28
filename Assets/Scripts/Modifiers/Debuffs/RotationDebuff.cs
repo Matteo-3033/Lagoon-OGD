bcba7e3da1324f91a3299e11d14bc2713169c043
Matteo Manzoni <matteo.manzoni2@studenti.unimi.it>
2024-05-28 10:39:29 +0200

    rotation debuff

---
+++ Assets/Scripts/Modifiers/Debuffs/RotationDebuff.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Modifiers.Buffs
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/Buffs/Rotation", fileName = "RotationDebuff")]
+    public class RotationDebuff : StatsModifier
+    {
+        public float percentDecrease = 25;
+        
+        public override void Enable()
+        {
+            Player.LocalPlayer.RotationController.SubPercentage(percentDecrease);
+        }
+
+        public override void Disable()
+        {
+            Player.LocalPlayer.RotationController.AddPercentage(percentDecrease);
+        }
+    }
+}
