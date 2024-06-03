b1f7c0457a2d72882ae591ca383aae4f63541263
ChristianPozzoli <pozzoli.christian99@gmail.com>
2024-06-03 23:11:25 +0200

    Disable field of view computation for non players

--- Assets/Scripts/Player/FoV/FieldOfView.cs
+++ Assets/Scripts/Player/FoV/FieldOfView.cs
@@ -7,12 +7,21 @@ using UnityEngine.UIElements;
 [RequireComponent(typeof(MeshFilter)), RequireComponent(typeof(MeshRenderer))]
 public class FieldOfView : NetworkBehaviour
 {
-    [SyncVar(hook = nameof(OnFoVDegreeChanged)), SerializeField] private float fieldOfViewDegree = 120F;
-    [SyncVar(hook = nameof(OnViewDistanceChanged)), SerializeField] private float viewDistance = 10F;
+    [SyncVar(hook = nameof(OnFoVDegreeChanged)), SerializeField]
+    private float fieldOfViewDegree = 120F;
+
+    [SyncVar(hook = nameof(OnViewDistanceChanged)), SerializeField]
+    private float viewDistance = 10F;
+
     [SyncVar, SerializeField] private int rayCount = 60;
-    
-    public bool CanSeePlayer { get; private set; }  // L'avversario nel caso il proprietario del field of view sia il player, altrimenti uno dei due giocatori
-    
+
+    public bool
+        CanSeePlayer
+    {
+        get;
+        private set;
+    } // L'avversario nel caso il proprietario del field of view sia il player, altrimenti uno dei due giocatori
+
     public class FieldOfViewArgs
     {
         public float FieldOfViewDegree;
@@ -20,7 +29,7 @@ public class FieldOfView : NetworkBehaviour
     }
 
     public event EventHandler<FieldOfViewArgs> OnFieldOfViewChanged;
-    
+
     private float AngleIncrease => fieldOfViewDegree / rayCount;
     private Vector3 Origin => Vector3.zero;
 
@@ -37,26 +46,26 @@ public class FieldOfView : NetworkBehaviour
     public override void OnStartClient()
     {
         base.OnStartServer();
-        
+
         OnFoVUpdated();
     }
-    
+
     private void Update()
     {
-        if (isClient && player && Player.Opponent?.transform == transform.root)
+        if ((isClient && player && Player.Opponent?.transform == transform.root) || !player)
             return;
-        
-        var vertices = new Vector3[rayCount + 2];   // +2 for the origin and the last vertex
+
+        var vertices = new Vector3[rayCount + 2]; // +2 for the origin and the last vertex
         var uv = new Vector2[vertices.Length];
         var triangles = new int[rayCount * 3];
-        
+
         vertices[0] = Origin;
 
         var startAngle = Quaternion.AngleAxis(-fieldOfViewDegree / 2, Vector3.up);
         var direction = startAngle * transform.forward;
 
         var canSeePlayer = false;
-        
+
         for (var i = 0; i <= rayCount; i++)
         {
             Vector3 newVertex;
@@ -78,32 +87,32 @@ public class FieldOfView : NetworkBehaviour
 
             direction = Quaternion.AngleAxis(AngleIncrease, Vector3.up) * direction;
         }
-        
+
         CanSeePlayer = canSeePlayer;
-        
+
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.uv = uv;
         mesh.triangles = triangles;
     }
-    
+
     public float GetViewDistance()
     {
         return viewDistance;
     }
-    
+
     public void SetViewDistance(float dist)
     {
         Debug.Log("Setting distance");
         viewDistance = dist;
         OnFoVUpdated();
     }
-    
+
     public float GetAngle()
     {
         return fieldOfViewDegree;
     }
-    
+
     public void SetAngle(float angle)
     {
         Debug.Log("Setting angle");
@@ -122,7 +131,7 @@ public class FieldOfView : NetworkBehaviour
         Debug.Log("Field of view degree changed");
         OnFoVUpdated();
     }
-    
+
     [ClientCallback]
     private void OnViewDistanceChanged(float oldValue, float newValue)
     {
@@ -133,7 +142,8 @@ public class FieldOfView : NetworkBehaviour
     [ClientCallback]
     private void OnFoVUpdated()
     {
-        OnFieldOfViewChanged?.Invoke(this, new FieldOfViewArgs {FieldOfViewDegree = fieldOfViewDegree, ViewDistance = viewDistance});
+        OnFieldOfViewChanged?.Invoke(this,
+            new FieldOfViewArgs { FieldOfViewDegree = fieldOfViewDegree, ViewDistance = viewDistance });
     }
 }
 
